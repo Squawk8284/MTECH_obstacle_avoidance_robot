ea82948329a7e77867953d6efa4ffc0ffc9155d0
kartik Sahasrabudhe <animeking3121997@gmail.com>
2025-01-29 14:45:44 +0530

    bezier

--- src/control_stack/include/robot.h
+++ src/control_stack/include/robot.h
@@ -1,79 +1,86 @@
-#ifndef __ROBOT__
-#define __ROBOT__
+// #ifndef __ROBOT__
+// #define __ROBOT__
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
-#include "nav_msgs/Odometry.h"
-#include "sensor_msgs/Imu.h"
-#include "tf/tf.h"
+// #include "ros/ros.h"
+// #include "std_msgs/String.h"
+// #include "geometry_msgs/Twist.h"
+// #include "nav_msgs/Odometry.h"
+// #include "sensor_msgs/Imu.h"
+// #include "tf/tf.h"
+// #include "tf/transform_listener.h"
+// #include "tf/transform_broadcaster.h"
+// #include "tf/transform_datatypes.h"
 
-#include "0xRobotcpplib.h"
-#include "robot_macros.h"
+// #include "0xRobotcpplib.h"
+// #include "robot_macros.h"
 
-class robot
-{
-public:
-    robot(ros::NodeHandle nh);
-    virtual ~robot();
+// class robot
+// {
+// public:
+//     robot(ros::NodeHandle nh);
+//     virtual ~robot();
 
-private:
-    int init();
-    void spin();
-    void publish();
+// private:
+//     int init();
+//     void spin();
+//     void publish();
 
-protected:
-    /******* ROBOT INSTANCE *******/
-    lib0xRobotCpp *RobotInt;
-    lib0xRobotCpp *IMUInt;
-    ros::NodeHandle robot_node_handle;
+// protected:
+//     /******* ROBOT INSTANCE *******/
+//     lib0xRobotCpp *RobotInt;
+//     lib0xRobotCpp *IMUInt;
+//     ros::NodeHandle robot_node_handle;
 
-    uint32 loopCount;
-    uint8 updateFrequency;
-
-
-    /******* PORT SETTINGS *******/
-    std::string robot_serial_port;
-    std::string imu_serial_port;
-
-    /******* DIMENSTION SETTINGS *******/
-    double wheel_diameter;
-    int counts_per_rev;
-    double axel_length;
-    double distance_per_count;
-
-    /******* IMU SETTINGS *******/
-    sensor_msgs::Imu imu_data;
-    ros::Publisher imuPub;
-
-    uint8 x_imu,y_imu,z_imu;
-    int16 gyroXYZ_Zero[3];
-    float gyro_angle;
-    double orientation_imu;
-
-    bool imu_init(void);
-    bool get_imu_data(sensor_msgs::Imu *imu_data);
+//     uint32 loopCount;
+//     uint8 updateFrequency;
 
 
-    /******* ODOM SETTINGS *******/
-    nav_msgs::Odometry position;
-    ros::Publisher pose_pub;
+//     /******* PORT SETTINGS *******/
+//     std::string robot_serial_port;
+//     std::string imu_serial_port;
 
-    int32 leftMotorCount, rightMotorCount, leftCountPrev, rightCountPrev, deltaLeftCount, deltaRightCount;
-    double velocity_x, velocity_y,velocity_theta;
-    double x_vel, y_vel, deltaX, deltaY, deltaUpdate;
-    double theta, deltaTheta, theta_Deg;
+//     /******* DIMENSTION SETTINGS *******/
+//     double wheel_diameter;
+//     int counts_per_rev;
+//     double axel_length;
+//     double distance_per_count;
 
-    void get_position(nav_msgs::Odometry *position);
-    void setPoseCovariance(nav_msgs::Odometry *position);
-    void setTwistCovariance(nav_msgs::Odometry *position);
+//     /******* IMU SETTINGS *******/
+//     sensor_msgs::Imu imu_data;
+//     ros::Publisher imuPub;
+
+//     uint8 x_imu,y_imu,z_imu;
+//     int16 gyroXYZ_Zero[3];
+//     float gyro_angle;
+//     double orientation_imu;
+
+//     bool imu_init(void);
+//     bool get_imu_data(sensor_msgs::Imu *imu_data);
+
+//     void set_imu_linear_acc_covarience();
+//     void set_imu_angular_vel_covarience();
+//     void set_imu_orientation_covarience();
+
+
+//     /******* ODOM SETTINGS *******/
+//     nav_msgs::Odometry position;
+//     ros::Publisher pose_pub;
+
+//     int32 leftMotorCount, rightMotorCount, leftCountPrev, rightCountPrev, deltaLeftCount, deltaRightCount;
+//     double velocity_x, velocity_y,velocity_theta;
+//     double x_vel, y_vel, deltaX, deltaY, deltaUpdate;
+//     double theta, deltaTheta, theta_Deg;
+
+//     void get_position(nav_msgs::Odometry *position);
+//     void setPoseCovariance(nav_msgs::Odometry *position);
+//     void setTwistCovariance(nav_msgs::Odometry *position);
 
 
 
 
 
-};
+// };
 
-#endif // __ROBOT__
+// #endif // __ROBOT__
--- src/control_stack/src/main.cpp
+++ src/control_stack/src/main.cpp
@@ -1,15 +1,68 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <cmath>
+#include <vector>
+#include <cstring>
+
 #include <ros/ros.h>
 #include "0xRobotcpplib.h"
 
-#define ROS_NODE "RosRobotNode"
+#include "control.h"
+
+using namespace std;
+
+/* VARIABLE DECLARATIONS*/
+lib0xRobotCpp robot;
+
+void *hSerial = robot.connect_comm("/dev/ttyRobot");
+void *hIMU = robot.connect_comm("/dev/ttyIMU");
+
+RobotState state;
+double P[5][5]; // Covariance matrix
+
+double Q_k[5][5] = { // Noise Matrix (from Paper)
+    {0.1, 0, 0, 0, 0},
+    {0, 0.1, 0, 0, 0},
+    {0, 0, 0.1, 0, 0},
+    {0, 0, 0, 0.1, 0},
+    {0, 0, 0, 0, 0.1}};
+
+// Measurement noise covariance matrix R (from paper)
+double R_k[2][2] = {
+    {0.018, 0},
+    {0, 0.018}};
+
+vector<pair<double, double>> bezierCurve;
 
 int main(int argc, char **argv)
 {
-    /* code */
-    ros::init(argc, argv, ROS_NODE);
-    ros::NodeHandle nh(std::string("~")); // This allows the node to publish topics under its nodespace which in this case is ROS_NODE/...
+    vector<pair<double, double>> controlPoints = {{0, 0}, {1, 2}, {3, 3}, {4, 2}, {5, 0}};
+    bezierCurve = computeBezierCurve(controlPoints, 100);
+    ekf_init();
 
+    int pointIndex = 0;
+    while (pointIndex < bezierCurve.size())
+    {
+        ControlInput u;
+        robot.getRightMotorVelocity(hSerial, (signed short)u.omega_r);
+        robot.getLeftMotorVelocity(hSerial, (signed short)u.omega_l);
+
+        SensorMeasurement z;
+        robot.getAccelerometerX(hIMU, (int16 *)&z.v_imu);
+        robot.getGyroZ(hIMU, (int16 *)&z.omega_imu);
+
+        ekf_update(z);
+        ekf_predict(u, 0.1);
+
+        double targetX = bezierCurve[pointIndex].first;
+        double targetY = bezierCurve[pointIndex].second;
+        moveRobot(targetX, targetY);
+
+        if (hypot(state.x - targetX, state.y - targetY) < 0.1)
+        {
+            pointIndex++;
+        }
+    }
+    robot.stop(hSerial);
     return 0;
 }
